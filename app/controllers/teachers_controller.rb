6721f7eb5659036aa248c6ae0bed61f8d1e2d927
danielmoralesp <daniel_moralesp@hotmail.com>
2017-03-20 18:16:35 -0500

    styles and js in all layouts

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -10,6 +10,10 @@ class TeachersController < ApplicationController
     render :layout => 'landing'
   end
 
+  def show
+    @teacher = Teacher.find(params[:id])
+  end
+
   def new
     @teacher = Teacher.new
   end
