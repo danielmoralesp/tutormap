28a954b291e562f4f94c15d279ef4d3aa5fedabb
danielmoralesp <daniel_moralesp@hotmail.com>
2017-04-04 15:40:27 -0500

    remove error  migrations

--- db/migrate/20170321153530_remove_user_id_from_tutoring.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserIdFromTutoring < ActiveRecord::Migration
-  def change
-    remove_reference :tutorings, :user_id, index: true, foreign_key: true
-  end
-end
--- db/migrate/20170321153758_remove_teacher_id_from_tutoring.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTeacherIdFromTutoring < ActiveRecord::Migration
-  def change
-    remove_reference :tutorings, :teacher_id, index: true, foreign_key: true
-  end
-end
